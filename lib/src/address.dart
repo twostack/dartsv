fe681b1e48eaf09be22ffe1a0784791d15bebe4c
Stephan M. February <stephan@werkswinkel.com>
2019-12-24 16:55:00 +0800

    Completed API documentation for Address class

--- lib/src/address.dart
+++ lib/src/address.dart
@@ -8,23 +8,133 @@ import 'exceptions.dart';
 
 //TODO: No support P2SH addresses at the moment. I'll add when I need it.
 
+
+/// This class abstracts away the internals of address encoding and provides
+/// a convenient means to both encode and decode information from a bitcoin address.
+///
+/// Bitcoin addresses are a construct which facilitates interoperability
+/// between different wallets. I.e. an agreement amongst wallet providers to have a
+/// common means of sharing the hashed public key value needed to send someone bitcoin using one
+/// of the standard public-key-based transaction types.
+///
+/// The Address does not contain a public key, only a hashed value of a public key
+/// which is derived as explained below.
+///
+/// Bitcoin addresses are not part of the consensus rules of bitcoin.
+///
+/// Bitcoin addresses are encoded as follows
+/// * 1st byte - indicates the network type which is either MAINNET or TESTNET
+/// * next 20 bytes - the hash value computed by taking the `ripemd160(sha256(PUBLIC_KEY))`
+/// * last 4 bytes  - a checksum value taken from the first four bytes of sha256(sha256(previous_21_bytes))
+
 class Address {
   List<NetworkType> _networkTypes;
 
-//  SVPublicKey _publicKey;  //TODO: There is a possibility you're overloading this as both publicKeyHex *and* ripemd160(sha256(publicKeyHex)). Investigate !
   String _publicKeyHash;
   AddressType _addressType;
   NetworkType _networkType;
-
   int _version;
 
-  //address param must be base58encoded and checksummed
+  /// Constructs a new Address object
+  ///
+  /// [address] is the base58encoded bitcoin address.
+  ///
   Address(String address) {
       _fromBase58(address);
   }
 
-  String get address {
+
+  /// Constructs a new Address object from a public key.
+  ///
+  /// [hexPubKey] is the hexadecimal encoding of a public key.
+  ///
+  /// [networkType] is used to distinguish between MAINNET and TESTNET.
+  ///
+  /// Also see [NetworkType]
+  Address.fromHex(String hexPubKey, NetworkType networkType){
+      _createFromHex(hexPubKey, networkType);
+  }
+
+
+  /// Constructs a new Address object from a compressed public key value
+  ///
+  /// [pubkeyBytes] is a byte-buffer of a public key
+  ///
+  /// [networkType] is used to distinguish between MAINNET and TESTNET.
+  ///
+  /// Also see [NetworkType]
+  Address.fromCompressedPubKey(List<int> pubkeyBytes, NetworkType networkType) {
+      _createFromHex(HEX.encode(pubkeyBytes), networkType);
+      this._publicKeyHash = HEX.encode(hash160(pubkeyBytes));
+  }
+
+  /// Constructs a new Address object from a base58-encoded string.
+  ///
+  /// Base58-encoded strings are the "standard" means of sharing bitoin addresses amongst
+  /// wallets. This is typically done either using the string of directly, or by using a
+  /// QR-encoded form of this string.
+  ///
+  /// Typically, if someone is sharing their bitcoin address with you, this is the method
+  /// you would use to instantiate an [Address] object for use with [Transaction] objects.
+  ///
+  Address.fromBase58(String base58Address) {
+      if (base58Address.length != 25){
+          throw new AddressFormatException('Address should be 25 bytes long. Only [${base58Address.length}] bytes long.');
+      }
+
+      _fromBase58(base58Address);
+  }
+
+
+  /// Serialise this address object to a base58-encoded string
+  ///
+  /// Base58-encoded strings are the "standard" means of sharing bitoin addresses amongst
+  /// wallets. This is typically done either using the string of directly, or by using a
+  /// QR-encoded form of this string.
+  ///
+  /// When sharing a bitcoin address with an external party either as a QR-code or via
+  /// email etc., this would typically be the form in which you share the address.
+  ///
+  String toBase58() {
+      // A stringified buffer is:
+      //   1 byte version + data bytes + 4 bytes check code (a truncated hash)
+      List<int> rawHash = HEX.decode(this._publicKeyHash).map((elem) => elem.toSigned(8)).toList();
+
+      return _getEncoded(rawHash);
+  }
+
+  /// Serialise this address object to a base58-encoded string.
+  /// This method is an alias for the [toBase58()] method
+  String toString() {
+      return toBase58();
+  }
+
+  /// Returns the public key hash [ripemd160(sha256(public_key))] encoded as a  hexadecimal string
+  String toHex(){
       return this._publicKeyHash;
+  }
+
+
+  String _getEncoded(List<int> hashAddress) {
+
+      List<int> addressBytes = List<int>(1 + hashAddress.length + 4);
+      addressBytes[0] = this._version;
+
+      //copy all of raw address content, taking care not to
+      //overwrite the version byte at start
+      addressBytes.fillRange(1, addressBytes.length, 0);
+      addressBytes.setRange(1, hashAddress.length + 1, hashAddress);
+
+      //checksum calculation...
+      //doubleSha everything except the last four checksum bytes
+      var doubleShaAddr = sha256Twice(addressBytes.sublist(0, hashAddress.length + 1));
+      var checksum = doubleShaAddr.sublist(0, 4).map((elem) => elem.toSigned(8)).toList();
+
+      addressBytes.setRange(hashAddress.length + 1, addressBytes.length, checksum);
+      var encoded = bs58check.encode(addressBytes);
+      var utf8Decoded = utf8.decode(encoded);
+
+      return utf8Decoded;
   }
 
   _fromBase58(String address){
@@ -58,78 +168,40 @@ class Address {
       this._networkType = networkType;
   }
 
-  Address.fromHex(String hexPubKey, NetworkType networkType){
-      _createFromHex(hexPubKey, networkType);
-  }
 
 
-  //TODO: WHAAAAA
-  Address.fromCompressedPubKey(List<int> pubkeyBytes, NetworkType networkType) {
-      _createFromHex(HEX.encode(pubkeyBytes), networkType);
-      this._publicKeyHash = HEX.encode(hash160(pubkeyBytes));
-  }
+  /// Returns a hash of the Public Key
+  ///
+  /// The sha256 digest of the public key is computed, and the result of that
+  /// computation is then passed to the [ripemd160()] digest function.
+  ///
+  /// The returned value is HEX-encoded
+  String get address => this._publicKeyHash;
 
-  Address.fromBase58(base58Address) {
-    if (base58Address.length != 25){
-        throw new AddressFormatException('Address should be 25 bytes long. Only [${base58Address.length}] bytes long.');
-    }
+  /// An alias for the [address] property
+  String get pubkeyHash160 => this._publicKeyHash;
 
-    _fromBase58(base58Address);
-  }
 
-  String toBase58() {
-    // A stringified buffer is:
-    //   1 byte version + data bytes + 4 bytes check code (a truncated hash)
-    List<int> rawHash = HEX.decode(this._publicKeyHash).map((elem) => elem.toSigned(8)).toList();
+  /// Returns a list of network types supported by this address
+  ///
+  /// This is only really needed because BSV has three different test networks
+  /// which technically share the same integer value when encoded, but for
+  /// which it is useful to have a type-level distinction during development
+  List<NetworkType> get networkTypes => this._networkTypes;
 
-    return _getEncoded(rawHash);
-  }
 
-  String toString() {
+  /// Returns the specific Network Type that this Address is compatible with
+  NetworkType get networkType => this._networkType;
 
-      List<int> rawAddress = HEX.decode(this._publicKeyHash).map((elem) => elem.toSigned(8)).toList();
-
-      //ripemd160(sha256(this._publicKey))
-//      var hashAddress = hash160(rawAddress);
-
-      return _getEncoded(rawAddress);
-
-  }
-
-  /// @returns : Public Key Hash as a HEX String
-  String toHex(){
-      return this._publicKeyHash;
-  }
-
-  get networkTypes => this._networkTypes;
-  get networkType => this._networkType;
-
-  get addressType => this._addressType;
-
-  /// @returns : hash160 value of the address. Not the publicly shareable address
-  get pubkeyHash160 => this._publicKeyHash;
-
-  String _getEncoded(List<int> hashAddress) {
-
-      List<int> addressBytes = List<int>(1 + hashAddress.length + 4);
-      addressBytes[0] = this._version;
-
-      //copy all of raw address content, taking care not to
-      //overwrite the version byte at start
-      addressBytes.fillRange(1, addressBytes.length, 0);
-      addressBytes.setRange(1, hashAddress.length + 1, hashAddress);
-
-      //checksum calculation...
-      //doubleSha everything except the last four checksum bytes
-      var doubleShaAddr = sha256Twice(addressBytes.sublist(0, hashAddress.length + 1));
-      var checksum = doubleShaAddr.sublist(0, 4).map((elem) => elem.toSigned(8)).toList();
-
-      addressBytes.setRange(hashAddress.length + 1, addressBytes.length, checksum);
-      var encoded = bs58check.encode(addressBytes);
-      var utf8Decoded = utf8.decode(encoded);
-
-      return utf8Decoded;
-  }
+  /// Returns the type of "standard transaction" this Address is meant to be used for.
+  ///
+  /// Addresses are not part of the consensus rules of bitcoin. However with the introduction
+  /// of "standard transaction types" wallets have fallen in line with providing
+  /// address types that distinguish the types of transactions the coins are meant
+  /// to be associated with
+  ///
+  /// See documentation for [Transaction]
+  AddressType get addressType => this._addressType;
 
 
 }
