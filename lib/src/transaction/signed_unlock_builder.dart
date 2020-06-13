3f141fd545a6f4114ad1367ee290bcee2372f212
Stephan M. February <stephan@werkswinkel.com>
2020-06-13 23:45:08 +0800

    P2PK Fixes
    - Fixes to the general framework to finally remove the default spending
      to P2PKH.
    - Fix to Transaction.fromHex() which resulted in incorrect internal
      TransactionID, which resulted in a bad spending TX
    - Fixed P2PK spending
    - Removed redundant code from TransactionInput related to old way of
      creating locking scripts

--- lib/src/transaction/signed_unlock_builder.dart
+++ lib/src/transaction/signed_unlock_builder.dart
@@ -1,7 +1,31 @@
 
 import '../../dartsv.dart';
 
+/// All unlocking scripts that will use OP_CHECKSIG or OP_CHECKMULTISIG operations
+/// must implement this interface. Implementing this interface will signal to
+/// the framework that the unlocking script (scriptSig) will participate in
+/// the process of generating signatures for the [Transaction]'s Input Script.
+///
+/// This interface will *always* need to be implemented alongside the
+/// [UnlockingScriptBuilder] interface.
+///
+/// After composing the [Transaction], when the developer calls the
+/// Transaction.signInput() method, the framework will inject the resultant
+/// signature into the [signatures] property of the
+/// [SignedUnlockBuilder] instance. This will allow the developer to use these
+/// signatures in generating the appropriate Input Script when the subclasses'
+/// [UnlockingScriptBuilder.getScriptSig()] method is called by either the
+/// framework, or explicitly by the developer themselves.
+///
 abstract class SignedUnlockBuilder {
+
+  /// The [signatures] property defines a list of signatures injected by
+  /// the framework whenever the Transaction.signInput() method is called.
+  /// Multiple sequential calls to Transaction.signInput() will cause
+  /// additional signatures to be added to this list.
+  ///
   List<SVSignature> get signatures;
+
+
   set signatures(List<SVSignature> value);
 }
--- test/transaction/p2pk_builder_test.dart
+++ test/transaction/p2pk_builder_test.dart
@@ -1,6 +1,10 @@
 
+import 'package:dartsv/src/address.dart';
+import 'package:dartsv/src/privatekey.dart';
 import 'package:dartsv/src/publickey.dart';
+import 'package:dartsv/src/sighash.dart';
 import 'package:dartsv/src/transaction/p2pk_builder.dart';
+import 'package:dartsv/src/transaction/transaction.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -11,6 +15,34 @@ void main() {
       var script = lockBuilder.getScriptPubkey();
       expect(script, isNotNull);
       expect(script.toString(), equals('33 0x022df8750480ad5b26950b25c7ba79d3e37d75f640f8e5d9bcd5b150a0f85014da OP_CHECKSIG'));
+    });
+
+    test('', (){
+      var coinbaseOutput = "02000000016b748661a108dc35d8868a9a552b9364c6ee3f06a4604f722882d49cdc4d13020000000048473044022073062451397fb5e7e2e02f1603e2a92677d516a5e747b1ae2ad0996387916d4302200ae2ec97d4525621cef07f75f0b92b5e83341761fa604c83daf0390a76d5024241feffffff0200e1f505000000001976a91494837d2d5d6106aa97db38957dcc294181ee91e988ac00021024010000001976a9144d991c88b4fd954ea62aa7182d3b3e251896a83188acd5000000";
+      var privateKey = SVPrivateKey.fromWIF("cVVvUsNHhbrgd7aW3gnuGo2qJM45LhHhTCVXrDSJDDcNGE6qmyCs");
+      var changeAddress = Address("mu4DpTaD75nheE4z5CQazqm1ivej1vzL4L"); // my address
+      var recipientAddress = Address("n3aZKucfWmXeXhX13MREQQnqNfbrWiYKtg"); //bitcoin-cli address
+
+      //Create a Transaction instance from the RAW transaction data create by bitcoin-cli.
+      //this transaction contains the UTXO we are interested in
+      var txWithUTXO = Transaction.fromHex(coinbaseOutput);
+
+      //Let's create the set of Spending Transaction Inputs. These Transaction Inputs need to refer to the Outputs in
+      //the Transaction we are spending from.
+      var utxo = txWithUTXO.outputs[0]; //looking at the decoded JSON we can see that our UTXO in at vout[0]
+      var pubkey = SVPublicKey.fromHex('022df8750480ad5b26950b25c7ba79d3e37d75f640f8e5d9bcd5b150a0f85014da');
+      var lockBuilder = P2PKLockBuilder(pubkey);
+
+      var locker = P2PKLockBuilder(pubkey);
+      var unlocker = P2PKUnlockBuilder();
+      var txn = Transaction();
+      txn.spendFromOutput(utxo, Transaction.NLOCKTIME_MAX_VALUE, scriptBuilder: unlocker) //set global sequenceNumber/nLocktime time for each Input created
+          .spendTo(recipientAddress, BigInt.from(50000000),scriptBuilder: locker) //spend half of a bitcoin (we should have 1 in the UTXO)
+          .sendChangeTo(changeAddress,scriptBuilder: locker) // spend change to myself
+          .withFeePerKb(100000);
+
+      //Sign the Transaction Input
+      txn.signInput(0, privateKey, sighashType: SighashType.SIGHASH_ALL | SighashType.SIGHASH_FORKID);
     });
   });
 }
