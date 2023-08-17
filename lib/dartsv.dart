c19654f9fbb763175fe7a071173d27dc36bdab76
Stephan February <stephan@frobots.io>
2023-08-17 21:04:57 +0800

    Example Pre-image test and exports
    - added an example on how to use the sighash preimage to spend an OP_PUSH_TX output
    - added additional useful exports to the library code
    - removed old wifcodec stub

--- lib/dartsv.dart
+++ lib/dartsv.dart
@@ -3,6 +3,8 @@
 /// More dartdocs go here.
 library dartsv;
 
+export 'src/exceptions.dart';
+export 'src/coin.dart';
 export 'src/address.dart';
 export 'src/hdpublickey.dart';
 export 'src/hdprivatekey.dart';
@@ -12,6 +14,7 @@ export 'src/privatekey.dart';
 export 'src/publickey.dart';
 export 'src/sighash.dart';
 export 'src/signature.dart';
+export 'src/varint.dart';
 export 'src/bip39/bip39.dart';
 export 'src/block/block.dart';
 export 'src/block/blockheader.dart';
@@ -22,6 +25,9 @@ export 'src/script/stack.dart';
 export 'src/script/script_error.dart';
 export 'src/script/script_chunk.dart';
 export 'src/script/scriptflags.dart';
+export 'src/script/script_pattern.dart';
+export 'src/script/stack.dart';
+export 'src/script/interpreter.dart';
 export 'src/transaction/transaction.dart';
 export 'src/transaction/transaction_builder.dart';
 export 'src/transaction/script_builder.dart';
@@ -30,17 +36,18 @@ export 'src/transaction/transaction_signer.dart';
 export 'src/transaction/transaction_input.dart';
 export 'src/transaction/transaction_output.dart';
 export 'src/transaction/transaction_input.dart';
-export 'src/encoding/base58check.dart';
 export 'src/transaction/locking_script_builder.dart';
 export 'src/transaction/unlocking_script_builder.dart';
 export 'src/transaction/p2pk_builder.dart';
 export 'src/transaction/p2pkh_builder.dart';
+export 'src/transaction/p2pkh_data_builder.dart';
 export 'src/transaction/p2ms_builder.dart';
 export 'src/transaction/p2sh_builder.dart';
+export 'src/transaction/default_builder.dart';
 export 'src/transaction/unspendable_data_builder.dart';
 export 'src/encoding/utils.dart';
-export 'src/exceptions.dart';
-export 'src/coin.dart';
+export 'src/encoding/base58check.dart';
 export 'src/crypto/ecies.dart';
+export 'src/crypto/hdutils.dart';
 
 
